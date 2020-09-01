8d8060b89a910a3e7f86cc672dbbbd2dc1da4cc5
Elena Tsypileva <etsypileva@gmail.com>
2020-09-01 17:36:05 +0300

    Added files

---
+++ src/main/java/ru/netology/statistic/StatsService.java
@@ -0,0 +1,54 @@
+package ru.netology.statistic;
+
+public class StatsService {
+    public int shouldCalculateTotalSalesVolume(int[] sales) {
+        int total = 0;
+        for (int sale : sales) {
+            total += sale;
+        }
+        return total;
+    }
+
+    public int shouldCalculateAverageSalesVolume(int total) {
+        int average = total / 12;
+
+        return average;
+    }
+
+    public int shouldCalculateMonthWithMinimum(int[] sales) {
+        int minimum = sales[0];
+        int indexForMinimum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            int score = sales[i];
+            if (minimum > score) {
+                minimum = score;
+                indexForMinimum = i;
+            }
+        }
+        int monthnumber = indexForMinimum + 1;
+
+        return monthnumber;
+    }
+    public int shouldCalculateAmountOfMonthsUnderAverage(int[] sales) {
+        int amount = 0;
+        for (int sale : sales) {
+            if (sale < 15) {
+                amount = amount + 1;
+            }
+        }
+
+        return amount;
+    }
+
+    public int shouldCalculateAmountOfMonthsAboveAverage(int[] sales) {
+        int amount = 0;
+        for (int sale : sales) {
+            if (sale > 15) {
+                amount = amount + 1;
+            }
+        }
+
+        return amount;
+    }
+
+}
---
+++ src/test/java/ru/netology/statistic/StatsServiceTest.java
@@ -0,0 +1,61 @@
+package ru.netology.statistic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateTotalSalesVolume() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
+        int actual = service.shouldCalculateTotalSalesVolume(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateAverageSalesVolume() {
+        StatsService service = new StatsService();
+        int total = 180;
+        int expected = 15;
+
+        int actual = service.shouldCalculateAverageSalesVolume(total);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateMonthWithMinimum() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.shouldCalculateMonthWithMinimum(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateAmountOfMonthsUnderAverage() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.shouldCalculateAmountOfMonthsUnderAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateAmountOfMonthsAboveAverage() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.shouldCalculateAmountOfMonthsAboveAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+}
